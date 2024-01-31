4cf547d5fcc978f295b9e05c3a935e1fdf3308bb
amirhosein <amirhosein8292@gmail.com>
2024-02-01 02:27:52 +0330

    write codes in app.js amir_branch

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function myFunc() {
   return true;
 }
+
+let x = 12;
+if (x === 12) {
+  console.log("x is true");
+} else {
+  alert("x is false");
+}
+function amirRet() {
+  return x + 12 / 11;
+}
+amirRet();
